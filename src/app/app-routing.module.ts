009b36e46adf5342f2de4b172429e908bbc38613
om202 <opsworld.g@gmail.com>
2021-04-02 22:53:27 -0600

    creating structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+
+  {
+    path: 'editor',
+    loadChildren: () =>
+      import('./editor/editor.module').then((m) => m.EditorModule),
+  },
+
+  {
+    path: '',
+    redirectTo: '/home', pathMatch: 'full'
+  },
+
+  {
+    path: '**',
+    component: PagenotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/editor/editor.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EditorRoutingModule } from './editor-routing.module';
+import { EditorComponent } from './editor.component';
+
+
+@NgModule({
+  declarations: [
+    EditorComponent
+  ],
+  imports: [
+    CommonModule,
+    EditorRoutingModule
+  ]
+})
+export class EditorModule { }
